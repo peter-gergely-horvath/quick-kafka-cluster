9cffcda10482d7446aab9986b8d5eb7b899c2aed
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2021-03-28 14:33:57 +0200

    Implemented basic Java installation

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Quick local Kafka development cluster
+
+This repository contains Vagrant and Ansbile configuration for quickly 
+setting up a small Kafka cluster on a local machine.
+
+Use `vagrant up` to start it.
+
+## Prerequisites: 
+
+Install the following software:
+ * HashiCorp Vagrant
+ * Ansible
+ * Oracle VirtualBox 
+ * `bsdtar` utility
---
+++ vagrant-halt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# set and IFS was taken from Microsoft Azure CLI templates.
+# -e: immediately exit if any command has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+
+vagrant halt
