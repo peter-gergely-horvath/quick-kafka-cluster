408a1632ee976df22dd3f80ac2e91fa29562ba14
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2021-04-13 16:31:32 +0200

    We no longer use any of the community.general packages, so do not install them

--- vagrant-up.sh
+++ vagrant-up.sh
@@ -7,6 +7,4 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-ansible-galaxy collection install community.general
-
 vagrant up
